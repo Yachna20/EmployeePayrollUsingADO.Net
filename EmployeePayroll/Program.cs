11362f800363a7e0279e2a9f4b6dbb14734d7435
Yachna20 <thakreyachna20@gmail.com>
2023-01-16 17:23:01 +0530

    [Yachna]-Ability to create EmployeePayroll database using ADO.Net

---
+++ EmployeePayroll/Program.cs
@@ -0,0 +1,18 @@
+﻿using EmployeePayroll.Repository;
+using System;
+namespace EmployeePayroll
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+             {
+                   
+                EmployeeRepository employee = new EmployeeRepository();
+                employee.CreateDatabase();
+             }
+            
+        }
+    }
+}
+
