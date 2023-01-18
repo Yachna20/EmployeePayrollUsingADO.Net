039b976d2fb0e34e0fba4b9b3d770d368c6fbb22
Yachna20 <thakreyachna20@gmail.com>
2023-01-18 15:10:46 +0530

    [Yachna]-Retrieve All Data And Check connection Using ADO.net

---
+++ EmployeePayroll/Model/EmployeeModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayroll.Model
+{
+    public class EmployeeModel
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Address { get; set; }
+        
+        public string Gender { get; set; }
+        public double Basic_Pay { get; set; }
+        public double Deduction { get; set; }
+        public double TaxablePay { get; set; }
+        public double Tax { get; set; }
+        public double Net_Pay { get; set; }
+    }
+}
